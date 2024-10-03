1c3e8200f540afa2d97aa08961ba4075b90109df
Daniel Amar <danielamarble123456@gmail.com>
2024-10-03 15:56:35 -0400

    Add rudimentary clock

--- static/clock.js
+++ static/clock.js
@@ -1,16 +1,21 @@
 function setMinuteHand() {
     const now = new Date();
     const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
+    console.log("minutes")
+    console.log(minutes)
   
     // Calculate the degrees for the minute hand
-    const minutesFraction = (minutes + seconds / 60) / 60;
-    const minuteDegrees = minutesFraction * 360 - 90; // Adjust by -90deg to set 12 o'clock at -90deg
+    const minutesFraction = minutes / 60;
+    console.log("Minutes fracion:")
+    console.log(minutesFraction)
+  
+    const minuteDegrees = minutesFraction * 360; // Adjust by -90deg to set 12 o'clock at -90deg
   
     const minuteHand = document.querySelector('.minute-hand');
     minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
   }
   
-  // Initialize and update every second
+  // Initialize and update every minute
   setMinuteHand(); // Initial call
-  setInterval(setMinuteHand, 1000); // Update every second
+  setInterval(setMinuteHand, 60000); // Update every minute
+  
