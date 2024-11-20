bfa01c5b9d288fef61624a8988ef66b5245c1d05
Daniel Amar <danielamarble123456@gmail.com>
2024-11-20 01:10:36 -0500

    Add back nginx container. Get certbot to generate ssl certs for configurations based on nginx/config/. Essentually containerize the certificate creation and usage process. Change frontend to use nginx route instead of tailscale network. Add back CORS.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,7 +8,7 @@ import { StrictMode } from "react"
 import { OpenAPI } from "./client"
 import theme from "./theme"
 
-OpenAPI.BASE = "http://localhost:8483"
+OpenAPI.BASE = "https://always12.duckdns.org"
 OpenAPI.TOKEN = async () => {
   return localStorage.getItem("access_token") || ""
 }
